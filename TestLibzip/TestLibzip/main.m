7135a28366d80285401d017ab430f37b36132169
kingly <kingly09@gmail.com>
2015-12-18 14:54:15 +0800

    添加一个测试文件

---
+++ TestLibzip/TestLibzip/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestLibzip
+//
+//  Created by kingly on 15/12/18.
+//  Copyright © 2015年 kingly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
