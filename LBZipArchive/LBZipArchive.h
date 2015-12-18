d4b35578530808928c6b0e268f1bd36a323b1030
kingly <kingly09@gmail.com>
2015-12-18 12:08:47 +0800

    初始化

---
+++ LBZipArchive/LBZipArchive.h
@@ -0,0 +1,13 @@
+//
+//  LBZipArchive.h
+//  LBZipArchiveDemo
+//
+//  Created by kingly on 15/12/18.
+//  Copyright © 2015年 https://github.com/kingly09/LBZipArchive . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LBZipArchive : NSObject
+
+@end
---
+++ LBZipArchive/LBZipArchive.m
@@ -0,0 +1,13 @@
+//
+//  LBZipArchive.m
+//  LBZipArchiveDemo
+//
+//  Created by kingly on 15/12/18.
+//  Copyright © 2015年 https://github.com/kingly09/LBZipArchive . All rights reserved.
+//
+
+#import "LBZipArchive.h"
+
+@implementation LBZipArchive
+
+@end
